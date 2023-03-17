38c435297a407336a6864b3946ec4bdb0f958429
Robert Manders <robert.manders@bjss.com>
2023-03-17 13:37:31 +0000

    Image search function.

--- src/extension/imageSearch.js
+++ src/extension/imageSearch.js
@@ -1,1 +1,26 @@
-// Searching the images...
+const subscriptionKey = '227637ac26314945a3ff73024b16eeb3'
+const url = 'https://api.bing.microsoft.com/v7.0/images/search'
+
+export async function searchImages(searchTerm) {
+  const searchUrl = url + encodeURIComponent(searchTerm)
+
+  const results = {}
+  
+  await fetch(searchUrl, {
+    method: 'GET',
+    headers: {
+      'Ocp-Apim-Subscription-Key': subscriptionKey
+    }
+  }).then(response => response.json())
+    .then(data => {
+      results = data
+    })
+
+  let imageUrls = []
+  
+  results.value.forEach(value => {
+    imageUrls.push(value.contentUrl)
+  })
+
+  return imageUrls
+}
