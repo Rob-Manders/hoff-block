c49005635791dca236305ce188e04e3542e37d5c
Robert Manders <robert.manders@bjss.com>
2023-03-17 14:51:26 +0000

    Set up storage. Add storage hooks.

---
+++ src/extension/storage.js
@@ -0,0 +1,19 @@
+export function toggleAdBlocker() {
+  const currentState = getExtensionState('blockerToggleState')
+
+  chrome.storage.local.set({ blockerToggleState: !currentState }) 
+}
+
+export function updateSearchTerm(searchTerm) {
+  chrome.storage.local.set({ searchTerm: searchTerm })
+}
+
+export function getExtensionState(key) {
+  let state = 'No data...'
+
+  chrome.storage.local.get([key]).then(result => {
+    state = result
+  })
+
+  return state
+}
